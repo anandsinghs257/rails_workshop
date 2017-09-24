2f86b3754a22934559890c43905dc86b2ab22a34
anandsinghs257 <anandsinghs257@gmail.com>
2017-09-24 15:48:52 +0530

    edit validation

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,6 @@
 class ArticlesController < ApplicationController
   before_action :set_article, only: [:show, :edit, :update, :destroy]
- before_action :aunthenticate_author!, only:[:create,:update,:edit,:destroy]
+ before_action :authenticate_author!, only:[:create,:update,:edit,:destroy]
   # GET /articles
   # GET /articles.json
   def index
